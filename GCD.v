4a93ace44126f3995d7ff5f9880532ff3e0b2e57
nikanika0221 <60596671+nikanika0221@users.noreply.github.com>
2020-10-06 17:35:25 +0400

    Add files via upload

---
+++ GCD.v
@@ -0,0 +1,74 @@
+
+
+module GCD(
+    input rst, clk,
+    input [7:0] num_A,
+    input [7:0] num_B,
+    input button,
+    output reg [7:0] LED_out
+    );
+    
+    reg [7:0] tempa, tempb;
+    reg pulse;
+    reg [1:0] state;
+	 reg first_taken;
+    parameter  take = 0, calculate = 1, hold = 2;
+   
+   
+    initial begin
+        state = hold;
+		  LED_out <= 0;
+		  tempa <= 0;
+		  tempb <= 0;
+		  pulse <= 0;
+		  first_taken <= 0;
+    end
+     
+    
+    always@(posedge clk) begin
+		  if(button && !pulse && !rst) begin
+				state <= take;
+				pulse <= 1;
+		  end
+		  if(!button) pulse <= 0;
+        case(state)
+            take : begin
+					LED_out <= 0;
+					//convert to positive if negative
+					 if(!first_taken) begin
+						tempa <= num_A[7] == 1? ~num_A+1 : num_A;
+						first_taken <= 1;
+						state <= hold;
+					 end
+					 else begin
+						tempb <= num_B[7] == 1? ~num_B+1 : num_B;
+						state <= calculate;
+						first_taken <= 0;
+					 end	 
+					 
+            end
+				
+            calculate : begin  
+					if(tempa == 0) LED_out <= tempb;
+					else if(tempb == 0) LED_out <= tempa;
+					else if(tempa == tempb) LED_out <= tempa;
+					else if(tempa > tempb) tempa <= tempa-tempb;
+					else tempb <= tempb - tempa;
+					if(LED_out) state <= hold;
+				end
+            default begin
+					if(button && !pulse) state <= take;
+					else state <= hold;
+				end
+        endcase
+		  if(rst) begin
+				state <= hold;
+				tempa <= 0;
+				tempb <= 0;
+				first_taken <= 0;
+				LED_out <= 0;
+		  end
+
+    end
+        
+endmodule
---
+++ GCD_testbench.v
@@ -0,0 +1,44 @@
+
+
+module GCD_Testbench;
+
+    //inputs
+    reg rst;
+    reg clk;
+    reg [7:0] num_A;
+    reg [7:0] num_B;
+    reg button;
+    
+    //outputs
+    wire [7:0]LED_out;
+    
+    GCD uut(
+        .rst(rst),
+        .clk(clk),
+        .num_A(num_A),
+        .num_B(num_B),
+        .button(button),
+        .LED_out(LED_out)     
+    );
+	 always #50 button <= !button;
+    always #3 clk <= !clk;
+    initial begin
+    clk = 0;
+    rst = 0;
+    num_A = 8'b10010100;
+    num_B = 8'b00111100;
+    button = 0;
+	 #220;
+	 num_A = 22;
+    num_B = 55;
+	 #50;
+	 #200;
+	 num_A = 88;
+    num_B = 50;
+	 #200;
+	 rst = 1;
+	 #10;
+	 rst = 0;
+    end
+endmodule
+
